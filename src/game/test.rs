136bc16f6d111cbbb9172f68f38721e04b2d8f93
Sid Parikh <parikhsid16@gmail.com>
2024-01-25 15:32:26 -0500

    add tests

--- src/game/test.rs
+++ src/game/test.rs
@@ -33,4 +33,42 @@ mod test {
         puzzle.perform(Action::Right);
         assert_eq!(puzzle.zero_index, 0);
     }
+
+    #[test]
+    fn test_puzzle_is_unsolved() {
+        let puzzle = Puzzle::new(
+            3,
+            vec![
+                Tile { number: 0 },
+                Tile { number: 1 },
+                Tile { number: 3 },
+                Tile { number: 4 },
+                Tile { number: 5 },
+                Tile { number: 6 },
+                Tile { number: 7 },
+                Tile { number: 2 },
+                Tile { number: 8 },
+            ],
+        );
+        assert!(!puzzle.is_win(), "Unsolved puzzle is marked as solved");
+    }
+
+    #[test]
+    fn test_puzzle_is_solved() {
+        let puzzle = Puzzle::new(
+            3,
+            vec![
+                Tile { number: 1 },
+                Tile { number: 2 },
+                Tile { number: 3 },
+                Tile { number: 4 },
+                Tile { number: 5 },
+                Tile { number: 6 },
+                Tile { number: 7 },
+                Tile { number: 8 },
+                Tile { number: 0 },
+            ],
+        );
+        assert!(puzzle.is_win(), "Solved puzzle is marked as unsolved");
+    }
 }
